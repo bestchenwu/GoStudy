e09ee62fe91f2af557f29405761fbab98ecd10c9
chenwu <chenwu@meitunmama.com>
2021-06-11 19:20:45 +0800

    提交helloworld

--- main/HelloWord.go
+++ main/HelloWord.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello,world")
+	//var s string = ""
+	//for i :=1 ;i<len(os.Args);i++{
+	//	s+=" "+os.Args[i]
+	//}
+	//fmt.Println("s="+s)
+	s := ""
+	//t := ""
+	for _,arg := range os.Args[1:]{
+		s+=""+arg
+	}
+	fmt.Println("s="+s)
+	//fmt.Println("hello,world")
 }
