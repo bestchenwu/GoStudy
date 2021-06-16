c587e9cbe30f6064c3e5c9b87f8e918dad9ca434
chenwu <chenwu@meitunmama.com>
2021-06-16 19:14:27 +0800

    提交控制结构学习

--- main/HelloWord.go
+++ main/HelloWord.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
@@ -16,6 +17,23 @@ func main() {
 	for _,arg := range os.Args[1:]{
 		s+=""+arg
 	}
-	fmt.Println("s="+s)
+	//fmt.Println("s="+s)
+	//fmt.Println(strings.Join(os.Args[1:], " "))
+	//练习1.1
+	//for i:=1;i<len(os.Args);i++{
+	//	fmt.Println(i)
+	//	fmt.Println("value="+os.Args[i])
+	//}
+	//1.3 查找重复的行
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan(){
+		counts[input.Text()]++
+	}
+	for line,n := range counts{
+		if n>1{
+			fmt.Println("%d\t%s\n",n,line)
+		}
+	}
 	//fmt.Println("hello,world")
 }
---
+++ unit2/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+const bolingF = 212.3
+
+func main() {
+	var f = bolingF
+	var t = (f-50)/2
+	fmt.Printf("t=%g",t)
+}
